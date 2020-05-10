c044b9a56ac09b34b54d475d810a649bc12fc96f
himanshu8686 <him.sharma8686@gmail.com>
2020-05-10 14:07:09 +0530

    Controllers are added

--- api/Routes/orders.js
+++ api/Routes/orders.js
@@ -1,7 +1,5 @@
 const express = require("express");
-const createError = require('http-errors');
-const mongoose = require('mongoose');
-const Order = require('../Models/Order.model');
+const OrdersController = require('../Controllers/orders.controller');
 const checkAuth = require('../Middleware/check-auth');
 const router = express.Router();
 
@@ -9,128 +7,21 @@ const router = express.Router();
 /**
  *  getting all orders
  */
-router.get('/', checkAuth, async (req, res, next) => {
-    // console.log('getting all orders');
-    try {
-
-        const results = await Order.find({}, {
-                __v: 0 // field to be omitted 0 here is for remove 1 is for retained
-            })
-            .populate('product', {
-                __v: 0
-            }); // populate fills the product(from product schema 'product') object in orders object
-        if (results.length > 0) {
-            res.send({
-                "response_code": 200,
-                "message": "All Order fetched successfully",
-                "total_results": results.length,
-                results
-            });
-        } else {
-            res.status(404).json({
-                "response_code": 404,
-                "message": "No Orders to display",
-            });
-        }
-    } catch (error) {
-        console.log(error.message);
-        next(createError(res.status, error.message));
-    }
-
-});
+router.get('/', checkAuth, OrdersController.orders_get_all);
 
 /**
  *  creating order
  */
-router.post('/', checkAuth, async (req, res, next) => {
-    console.log('creating order');
-
-    try {
-        const order = new Order({
-            _id: mongoose.Types.ObjectId(),
-            quantity: req.body.quantity,
-            product: req.body.productId
-        });
-        const result = await order.save();
-        console.log(result);
-        res.send({
-            "response_code": 201,
-            "message": "Product created successfully",
-            result
-        });
-    } catch (error) {
-        console.log(error);
-        if (error instanceof mongoose.CastError) {
-            next(createError(400, 'invalid product id'));
-            return;
-        } else if (error.name === 'ValidationError') {
-            // this Validation error comes from mongoose
-            next(createError(422, 'Please provide proper product Id'));
-            return;
-        }
-        next(error);
-    }
-});
+router.post('/', checkAuth, OrdersController.createOrder);
 
 /**
  *  getting all orders by id
  */
-router.get('/:orderId', checkAuth, async (req, res, next) => {
-    // console.log('getting order by id');
-    try {
-        const id = req.params.orderId;
-        const order = await Order.findById(id, {
-                __v: 0
-            })
-            .populate('product', {
-                __v: 0
-            });
-        if (!order) {
-            throw createError(404, "Order does not exist for this id");
-        }
-        //console.log("from db", product);
-        res.send({
-            "response_code": 200,
-            "message": `Order find with ${id}`,
-            order
-        });
-    } catch (error) {
-        console.log(error.message);
-        if (error instanceof mongoose.CastError) {
-            next(createError(400, 'invalid order id'));
-            return;
-        }
-        next(error);
-    }
-});
+router.get('/:orderId', checkAuth, OrdersController.getOrderById);
 
 /**
  *  delete orders by id
  */
-router.delete('/:orderId', checkAuth, async (req, res, next) => {
-    console.log('deleting all order');
-    const id = req.params.orderId;
-    try {
-        const result = await Order.findOneAndDelete({
-            _id: id
-        });
-        if (!result) {
-            throw createError(404, 'Order does Not Exist');
-        }
-        console.log(result);
-        res.send({
-            "response_code": 200,
-            "message": `Order with ${id} deleted`,
-            result
-        });
-    } catch (error) {
-        console.log(error.message);
-        if (error instanceof mongoose.CastError) {
-            next(createError(400, 'invalid order id'));
-            return;
-        }
-        next(error);
-    }
-});
+router.delete('/:orderId', checkAuth, OrdersController.deleteOrderById);
 
 module.exports = router;
--- api/Routes/products.js
+++ api/Routes/products.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const createError = require('http-errors');
-const mongoose = require('mongoose');
-const Product = require('../Models/Product.model');
+
+
+const ProductController = require('../Controllers/products.controller');
 const multer = require('multer');
 const checkAuth = require('../Middleware/check-auth');
 
@@ -36,156 +37,26 @@ const router = express.Router();
 /**
  *  getting all Products
  */
-router.get('/', async (req, res, next) => {
-    // res.send('getting lists of all products');
-    try {
-        const results = await Product.find({}, {
-            __v: 0 // field to be omitted 0 here is for remove 1 is for retained
-        });
-
-        if (results.length > 0) {
-            res.send({
-                "response_code": 200,
-                "message": "Product fetched successfully",
-                "total_results": results.length,
-                results
-            });
-        } else {
-            res.status(404).json({
-                "response_code": 404,
-                "message": "No Product to display",
-            });
-        }
-
-    } catch (error) {
-        console.log(error.message);
-        next(createError(res.status, error.message));
-    }
-});
+router.get('/', ProductController.getAllProducts);
 
 /**
  *  creating a product
  */
-router.post('/', checkAuth, upload.single('productImage'), async (req, res, next) => {
-    console.log("file=>", req.file);
-    try {
-        const product = new Product({
-            _id: new mongoose.Types.ObjectId(),
-            name: req.body.name,
-            price: req.body.price,
-            productImage: `localhost:${process.env.PORT}\\` + req.file.path
-        });
-        const result = await product.save();
-        console.log('From database', result);
-        res.send({
-            "response_code": 201,
-            "message": "Product created successfully",
-            result
-        });
-    } catch (error) {
-        console.log(error.message);
-        // this Validation error comes from mongoose
-        if (error.name === 'ValidationError') {
-            next(createError(422, error.message));
-            return;
-        }
-        next(error);
-    }
-});
+router.post('/', checkAuth, upload.single('productImage'), ProductController.createProduct);
 
 /**
  * getting products by id
  */
-router.get('/:productId', async (req, res, next) => {
-    try {
-        const id = req.params.productId;
-        const product = await Product.findById(id, {
-            __v: 0
-        });
-        if (!product) {
-            throw createError(404, "Product does not exist for this id");
-        }
-        //console.log("from db", product);
-        res.send({
-            "response_code": 200,
-            "message": `Product find with ${id}`,
-            product
-        });
-    } catch (error) {
-        console.log(error.message);
-        if (error instanceof mongoose.CastError) {
-            next(createError(400, 'invalid product id'));
-            return;
-        }
-        next(error);
-    }
-
-});
+router.get('/:productId', ProductController.getProductById);
 
 /**
  *  update product by id
  */
-router.patch('/:productId', checkAuth, async (req, res, next) => {
-    // console.log('update product by id');
-    try {
-        const id = req.params.productId;
-        const updates = req.body;
-        // options will return the updated product
-        const options = {
-            new: true
-        };
-
-        try {
-            const result = await Product.findByIdAndUpdate(id, updates, options);
-            if (!result) {
-                throw createError(404, "Product does not exist");
-            }
-            res.send({
-                "response_code": 200,
-                "message": `Product updated successfully with ${id}`,
-                result
-            });
-        } catch (error) {
-            console.log(error.message);
-            if (error instanceof mongoose.CastError) {
-                next(createError(400, 'invalid product id'));
-                return;
-            }
-            next(error);
-        }
-
-    } catch (error) {
-
-    }
-});
+router.patch('/:productId', checkAuth, ProductController.updateProductById);
 
 /**
  * delete product by id
  */
-router.delete('/:productId', checkAuth, async (req, res, next) => {
-    // console.log('delete product by id');
-    const id = req.params.productId;
-    try {
-        const result = await Product.findOneAndDelete({
-            _id: id
-        });
-        if (!result) {
-            throw createError(404, 'Product does Not Exist');
-        }
-        console.log(result);
-        res.send({
-            "response_code": 200,
-            "message": `Product with ${id} deleted`,
-            result
-        });
-    } catch (error) {
-        console.log(error.message);
-        if (error instanceof mongoose.CastError) {
-            next(createError(400, 'invalid product id'));
-            return;
-        }
-        next(error);
-    }
-});
+router.delete('/:productId', checkAuth, ProductController.deleteProductById);
 
 module.exports = router;
